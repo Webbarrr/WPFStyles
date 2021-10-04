68da7f91d254a1c60a891e7f566eb4e58a82aa71
James <James@DESKTOP-1S9QB9R>
2021-10-04 11:43:24 +0100

    added targettype iftrue converter

---
+++ WPFStyles/Converters/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFStyles.Converters
+{
+    public class GameStateToBoolConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object trueValue, CultureInfo culture)
+        {
+            if (value != null && value.GetType().IsEnum)
+                return Equals(value, trueValue);
+            else
+                return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object trueValue, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+                return trueValue;
+            else
+                return DependencyProperty.UnsetValue;
+        }
+
+        #endregion IValueConverter Members
+    }
+}
